f2d7a1839da4ccaad88a9934869eef14d67130a6
nsaddmbek <saddambek.nur@gmail.com>
2020-05-25 12:37:39 +0600

    fix problem with comments

--- news/admin.py
+++ news/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from news.models import Post, Comment
 
-admin.site.register(Post)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
--- news/models.py
+++ news/models.py
@@ -8,10 +8,14 @@ class Post(models.Model):
     date = models.DateField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name='%(class)s')
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     comment = models.TextField()
